112e26635b3ba91952523297f2f6593e58c8ee12
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 20:58:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,31 +1,15 @@
 # Add  code here!
 def prime?(number)
-  iterateArray = (0..number).to_a
-  puts "Array = #{iterateArray}"
   puts "Number = #{number}"
   
-  iterateArray.each do |i|
-    puts "This is i: #{i}"
-    #if number % i == 0  
-    #  primeCounter += 1 
-    #  puts "Counter = #{primeCounter}"
-    #end
-  end
-  
-  #check if it is zero
-  #if number == 0 || number == 1
-   # false
+  if number == 0 || number == 1 
+    false
+  else
+    iterateArray = (2..number).to_a
+    puts "Array = #{iterateArray}"
     
-  #else #if not zero
-   # iterateArray.each do |i|
-      #if number % i == 0  
-      #  primeCounter += 1 
-      #end
-    #primeCounter = 1 
-    #if primeCounter > 1 #if it is prime return false
-     # false
-    #else 
-     # true #if it is not prime return true
-    #end
-  #end
+    iterateArray.each do |i|
+      puts "This is i: #{i}"
+    end
+  end
 end
