61d12a77d778a9221153536bcc0b00cb2c91104e
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 21:07:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,19 @@
 # Add  code here!
 def prime?(number)
-  puts "Number = #{number}"
-  primeCounter = 0 
+  dividesEven_Counter = 0 
   
-  if number < 2 
+  if number < 2       #if number is less than 2 not prime 
     false
-  else
-    iterateArray = (2..number).to_a
+  else                #if number is greater than 2 check prime
+    iterateArray = (2..number).to_a 
     
     iterateArray.each do |i|
       if number % i == 0 
-        primeCounter += 1 
+        dividesEven_Counter += 1 
       end
     end
     
-    if primeCounter > 1 
+    if dividesEven_Counter > 1 
       false
     else
       true 
