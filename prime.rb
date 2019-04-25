e9e423d7fcc896e69bdcd83cae0bb56fb773757d
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 21:05:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,24 +1,23 @@
 # Add  code here!
 def prime?(number)
   puts "Number = #{number}"
+  primeCounter = 0 
   
   if number < 2 
     false
   else
     iterateArray = (2..number).to_a
-    puts "Array = #{iterateArray}"
     
     iterateArray.each do |i|
-      puts "This is i: #{i}"
       if number % i == 0 
         primeCounter += 1 
       end
-      
-      if primeCounter > 1 
-        false
-      else
-        true 
-      end
+    end
+    
+    if primeCounter > 1 
+      false
+    else
+      true 
     end
   end
 end
