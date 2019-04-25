81fc3a86e66b86671443c6e5bca63864f5fad294
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 20:33:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,14 @@
 # Add  code here!
 def prime?(number)
   #check if it is zero
-  
-  #if not zero
+  if number == 0 
+    false
+  else #if not zero
+    if number == 1
+      true
+    else 
     #if it is prime return TRUE
   
     #if it is not prime return FALSE
+  end
 end
